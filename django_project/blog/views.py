c6b5a2ea212eb8463bbef3864062bbdad5001295
RyomenDev <aakashvatsh007@gmail.com>
2025-01-19 20:00:54 +0530

    paasing and using data in templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -5,8 +5,29 @@ from django.http import HttpResponse
 # def home(request):
 #     return HttpResponse('<h1>Blog Home</h1>')
 
+# def home(request):
+#     return render(request,'blog/temp.html')
+
+posts = [ # dictionary
+    {
+        'author': 'Tom',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 19, 2025'
+    },
+    {
+        'author': 'Jerry',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 17, 2025'
+    }
+]
+
 def home(request):
-    return render(request,'blog/temp.html')
+    context = { 
+        'posts': posts # dictionary
+    }
+    return render(request, 'blog/temp.html', context)
 
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
