2c19cd28d6db90f4ad4f6631c74330bb45af5bfc
RyomenDev <aakashvatsh007@gmail.com>
2025-01-21 09:41:35 +0530

    email input in form

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -4,26 +4,17 @@ from django.contrib import messages
 from .forms import UserRegisterForm
 
 # Create your views here.
-# def register(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save() # saving the form
-#             username = form.cleaned_data.get('username')
-#             messages.success(request, f'Account created for {username}!') # flash msg
-#             return redirect('blog-home')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'users/register.html', {'form': form})
-
 def register(request):
     if request.method == 'POST':
+        # form = UserCreationForm(request.POST)
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            form.save() # saving the form
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Account created for {username}!') # flash msg
             return redirect('blog-home')
     else:
+        # form = UserCreationForm()
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
