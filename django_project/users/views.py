44c2297e1ef62374f7abd834283a69ef97250502
RyomenDev <aakashvatsh007@gmail.com>
2025-01-21 08:40:56 +0530

    using .forms.py

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save() # saving the form
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Account created for {username}!') # flash msg
+#             return redirect('blog-home')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'users/register.html', {'form': form})
+
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save() # saving the form
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!') # flash msg
+            messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
