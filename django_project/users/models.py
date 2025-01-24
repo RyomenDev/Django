ab625d79d98e2c6ccc5f97e1ab9e5b55c6ffef98
RyomenDev <aakashvatsh007@gmail.com>
2025-01-24 22:08:39 +0530

    User model - dunder method

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -6,5 +6,5 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) # if user is deleted, delete profile , not vice versa
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-    def __str__(self):
+    def __str__(self): # dunder method to return string representation of object
         return f'{self.user.username} Profile'
